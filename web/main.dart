8d4f4f1c43691d7c6a7a66fa8d63a4f18d8f27df
John Ryan <ryjohn@google.com>
2020-02-03 13:45:00 -0800

    add copyright headers

--- web/main.dart
+++ web/main.dart
@@ -1,4 +1,9 @@
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:angular/angular.dart';
+
 import 'package:angular_router/angular_router.dart';
 import 'package:angular_routing_example/app_component.template.dart' as ng;
 
